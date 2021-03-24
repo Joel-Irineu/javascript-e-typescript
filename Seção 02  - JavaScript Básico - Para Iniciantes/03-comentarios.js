be83994d4009b5dec3758943e1ee39d635e74b29
Joel Irineu <irineu.joel01@gmail.com>
2021-03-24 14:55:22 -0300

    comentarios

---
+++ Seção 02  - JavaScript Básico - Para Iniciantes/03-comentarios.js
@@ -0,0 +1,10 @@
+// E escrever um comentario aqui
+console.log('olá, mundo!') // aqui tem outro comentario
+console.log('linha 1')
+// mais um
+// console.log('linha 2')
+/* 
+console.log('linha 3')
+console.log('linha 4')
+*/
+// aqui vai ser ignorado

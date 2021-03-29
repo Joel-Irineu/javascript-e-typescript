6dd148dc1365a7ef7c556bb2e468d508aef5c39d
Joel Irineu <irineu.joel01@gmail.com>
2021-03-29 18:41:44 -0300

    Constantes com const

---
+++ Seção 02  - JavaScript Básico - Para Iniciantes/05-constantes.js
@@ -0,0 +1,11 @@
+// Não podemos modificar o valor de uma constante
+// A constante deve ser declarada e atribuida no mesmo momento
+const nome = 'João';
+console.log(nome)
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+const resultadoTriplicado = resultado * 3;
+console.log(resultado, resultadoDuplicado, resultadoTriplicado)

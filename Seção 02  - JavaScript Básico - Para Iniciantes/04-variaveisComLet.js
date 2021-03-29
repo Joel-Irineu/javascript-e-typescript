bbddb612d26dd880caaf0fa49a490bfef1d2c6da
Joel Irineu <irineu.joel01@gmail.com>
2021-03-29 00:28:17 -0300

    Variáveis com let

---
+++ Seção 02  - JavaScript Básico - Para Iniciantes/04-variaveisComLet.js
@@ -0,0 +1,24 @@
+let nome = 'Henrique' // String
+console.log(nome, 'nasceu em 1984.')
+console.log('em 2000 ', nome,' conhesceu Maria.')
+console.log(nome ,' casou-se com Maria em 2015')
+console.log('Maria teve 1 filho com', nome, 'em 2015.')
+console.log('O filho de ', nome, 'se chama Eduardo')
+
+let outroNome; // Declaração da variavel
+outroNome = 'Joel'; // Inicialização da variavel
+console.log(outroNome);
+outroNome = 'Irineu'
+console.log(outroNome)
+
+/**
+ * REGRAS:
+ *  - Lets não podem ser redeclarados;
+ *  - Não pode utilizar palavra reservada;
+ *  - Precisa ter nomes significativos;
+ *  - Não pode começar o nome com numero;
+ *  - Não podem conter espaços ou traços;
+ *  - Utilizamos camelCase
+ *  - case-sensitive (Letra maiuscula e minuscula fazem diferença)
+ *  - utilizar let e não var
+*/

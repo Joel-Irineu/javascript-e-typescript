cdc1978b22c1f14746fccf8bbb79a4f9358e8b49
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-14 11:49:50 -0300

    Tipos de dados primitivos

---
+++ 01 - JS para iniciantes/tiposPrimitivos.js
@@ -0,0 +1,19 @@
+const nome = 'Joel'; // String
+const nome1 = "Joel"; // String
+const nome2 = `Joel`; // String
+
+const num1 = 10; // number
+const num2 = 10.52; // number
+
+let nomeAluno; // Undefined -> Não aponta para nenhum local na memoria
+let sobrenomeAluno = null // Null -> Não aponta para nenhum local na memoria
+
+const aprovado = true // Boolean -> true ou false
+
+
+console.log(typeof nome, nome);
+console.log(typeof num1, num1);
+console.log(typeof nomeAluno, nomeAluno);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
+console.log(typeof aprovado, aprovado);
+

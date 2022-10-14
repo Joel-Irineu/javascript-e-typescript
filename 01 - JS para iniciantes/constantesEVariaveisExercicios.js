f43b406fc0b8fa8bd9d28b58dbb8285a29b53560
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-14 11:30:30 -0300

    Constantes e variaveis exercicio

---
+++ 01 - JS para iniciantes/constantesEVariaveisExercicios.js
@@ -0,0 +1,11 @@
+let nome = 'Otávio';
+let idade = 30;
+let peso = 84;
+let altura = 1.8;
+const IMC = peso / (altura * altura);
+
+let anoNascimento = 2022 - idade;
+
+console.log(`${nome} tem ${idade}, pesa ${peso}kg`);
+console.log(`tem ${altura} de altura e seu IMC é de ${IMC}`);
+console.log(`${nome} nasceu em ${anoNascimento}`)

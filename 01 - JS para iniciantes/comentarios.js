f23df563cf4f838a080bfd9bd51645b9e089a54c
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-14 10:52:20 -0300

    Comentarios

---
+++ 01 - JS para iniciantes/comentarios.js
@@ -0,0 +1,13 @@
+// É escrever um comentário
+console.log('Hello world'); // Aqui é um comentario
+console.log('Linha 01');
+// Comentário
+console.log('Linha 02');
+/**
+ * Aqui é
+ * um bloco de
+ * comentarios
+ */
+console.log('Linha 03');
+// console.log('Linha 04');
+// Vai ser ignorado

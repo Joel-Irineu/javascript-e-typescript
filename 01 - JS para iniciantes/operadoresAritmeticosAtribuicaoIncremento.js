0cc020fa9bc6547af04b9b4acda8502cca1648cc
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-14 12:05:55 -0300

    operadores aritiméticos, de atribuição e de incremento

---
+++ 01 - JS para iniciantes/operadoresAritmeticosAtribuicaoIncremento.js
@@ -0,0 +1,29 @@
+const num1 = 10;
+const num2 = 5;
+
+console.log(num1 + num2); // Adição
+console.log(num1 - num2); // Subtração
+console.log(num1 * num2); // multiplicação
+console.log(num1 / num2); // divisão
+console.log(num1 ** num2); // potenciação
+console.log(num1 % num2); // modal
+
+/**
+ * Precedencia
+ * (), **, * / %, + -
+ */
+
+let contador = 1;
+contador++; // 2
+contador++; // 3
+contador++; // 4
+console.log(contador);
+
+contador--; // 3
+contador--; // 4
+contador--; // 1 
+console.log(contador);
+
+
+contador += 2; // atribuição mais calculo
+console.log(contador);

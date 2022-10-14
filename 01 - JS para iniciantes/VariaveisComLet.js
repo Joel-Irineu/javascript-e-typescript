79e7114b731b5390af979c170119cc291a74d291
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-14 11:11:42 -0300

    Variaveis com let

---
+++ 01 - JS para iniciantes/VariaveisComLet.js
@@ -0,0 +1,15 @@
+let nome; // Declarou a variavel
+nome = 'João'; // Inicializando a variavel 
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'Casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com', nome,'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
+
+// Não pode palavra rezervada
+// Precisa ter nome significativo
+// Não pode começar com números
+// Não pode conter espaço ou traços
+// Por convenção usamos camelCase
+// Case sensitive (Letras Maiusculas e minusculas tem diferença)

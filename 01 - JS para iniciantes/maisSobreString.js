9e8a2253abf028c02ce632d23d2fd1197ba8edad
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-14 15:16:18 -0300

    Mais sobre strings

---
+++ 01 - JS para iniciantes/maisSobreString.js
@@ -0,0 +1,16 @@
+let umaString = 'O rato roeu a roupa do rei de roma';
+
+console.log(umaString.concat('e a rainha com raiva remendou'));
+console.log(umaString.replace('rato', 'gato'));
+console.log(umaString.length);
+console.log(umaString.slice(2, 6));
+console.log(umaString.substring(2, 6));
+console.log(umaString.split(' '));
+console.log(umaString.toUpperCase());
+console.log(umaString.toLocaleLowerCase());
+
+
+
+
+
+

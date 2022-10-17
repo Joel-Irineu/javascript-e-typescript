68e6bda76ff4e27e33c0ee62bed5be9c91c2340e
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-17 11:48:16 -0300

    Objeto Math

---
+++ 01 - JS para iniciantes/objetoMath.js
@@ -0,0 +1,13 @@
+let num1 = 9.54578;
+
+let num2 = Math.floor(num1); // Arredonda para cima
+num2 = Math.ceil(num1); // Arredonda para baixo
+num2 = Math.round(num1); // Arredonda para o mais proximo
+
+console.log(num2);
+
+console.log(Math.max(1, 2, 3, 4, 5, 6, 7, -10, -50, 1500, 9, 8, 7)); // Retorna o maior numero
+
+console.log(Math.random()); // Retorna um numero aleatorio
+
+console.log(Math.PI); // Retorna PI

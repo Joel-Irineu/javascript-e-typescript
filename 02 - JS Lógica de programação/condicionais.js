988b6eaaced67b27bb18746baf23987133fa55a8
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-18 11:18:38 -0300

    mais sobre condicionais

--- 02 - JS Lógica de programação/condicionais.js
+++ 02 - JS Lógica de programação/condicionais.js
@@ -9,3 +9,14 @@ if(hora >= 0 && hora <= 11){
 }else{ // se retornar falso, faça...
     console.log('Olá!');
 }
+
+const numero = 10;
+if(numero >= 0 && numero <= 5){
+    console.log('Seu numero esta entre 0 e 5');
+}else if(numero >= 6 && numero <= 8){
+    console.log('Seu numero esta entre 6 e 8');
+}else if(numero >= 9 && numero <= 11){
+    console.log('Seu numero esta entre 9 e 11');
+}else{
+    console.log('Seu numero não esta ente 0 e 11');
+}

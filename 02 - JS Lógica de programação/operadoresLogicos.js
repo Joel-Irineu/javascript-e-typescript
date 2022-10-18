c738671a805bacfa18d74c82869a0b6fddfacacc
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-18 10:43:18 -0300

    Operadores Logicos

---
+++ 02 - JS Lógica de programação/operadoresLogicos.js
@@ -0,0 +1,3 @@
+console.log(true && true); // AND, e -> todos precisam ser veradeiras para retornar true
+console.log(true || false); //OR, ou -> ao menos uma precisa ser verdadeira pra retnornar true
+console.log(!true); //NOT, não, negação -> ela inverte o valor da expressão

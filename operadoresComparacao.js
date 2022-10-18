d03d04d78806188d52c92d664a31d6e9c8415826
Joel-Irineu <irineu.joel01@gmail.com>
2022-10-18 10:36:22 -0300

    Operadores de comparação

---
+++ operadoresComparacao.js
@@ -0,0 +1,8 @@
+console.group(10 > 5); // Maior que
+console.log(10 >= 10); // Maior ou igual a
+console.log(10 < 11); // Menor que
+console.log(11 <= 11); // Menor ou igual a
+console.log(10 == 10); // Igual a
+console.log(10 === '10'); //Estritamente igual a
+console.log(10 != '10'); //Diferente de
+console.log(10 !== '10'); // Estritamennte diferente de
